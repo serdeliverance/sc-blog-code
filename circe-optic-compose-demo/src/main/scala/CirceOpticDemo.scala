3bf8c69a3ff2fc5ba4a142fa13e1da844f5406b6
Sergio Cano <serdeliverance@gmail.com>
2020-08-16 21:10:22 -0300

    circe optics with fp

--- circe-optic-compose-demo/src/main/scala/CirceOpticDemo.scala
+++ circe-optic-compose-demo/src/main/scala/CirceOpticDemo.scala
@@ -1,7 +1,6 @@
-import CirceOpticDemo.JsonManipulationAPI.{setHotelType, setLocationAddress, setLocationZipCode, setName, setStars}
 import io.circe.Json
-import io.circe.parser._
 import io.circe.optics.JsonPath._
+import io.circe.parser._
 
 import scala.annotation.tailrec
 
@@ -14,21 +13,64 @@ object CirceOpticDemo extends App {
   val jsonStr =
     """
     {
-      "id" : 3305,
-      "active" : true,
-      "name" : "Voyage Dream",
-      "hotel_type" : "premium",
-      "stars" : 5,
-      "location" : {
-        "address" : "500 East A Street South",
-        "zipcode" : "69153-3111",
+      "id": 3305,
+      "active": true,
+      "name": "Voyage Dream",
+      "hotel_type": "premium",
+      "stars": 5,
+      "location": {
+        "address": "500 East A Street South",
+        "zipcode": "69153-3111"
       }
     }
     """
 
-  import JsonManipulationAPI._
-
   val json = parse(jsonStr)
+
+
+  // approach 0: using cursors
+  val doc = json.getOrElse(Json.Null)
+
+  val cursor = doc.hcursor
+
+  // modifying just one field using cursors
+
+//  val result = cursor
+//    .downField("name").withFocus(_.mapString(_ => "The Lost Wonder")).top
+//    .fold("parser error") (_.toString())
+
+  // multiple field modifications using cursors
+
+  val result0_0 = cursor
+    .downField("name").withFocus(_.mapString(_ => "The Lost Wonder")).top
+    .flatMap(json => json.hcursor.downField("hotel_type").withFocus(_.mapString(_ => "premium")).top)
+    .flatMap(json => json.hcursor.downField("stars").withFocus(_ => Json.fromInt(5)).top)
+    .fold("parser error")(_.toString())
+
+  // approach 0.1: defining transformations with cursors
+
+  def updateName(name: String): Json => Option[Json] =
+    json => json.hcursor.downField("name")
+              .withFocus(_.mapString(_ => name)).top
+  def updateHotelType(hotelType: String): Json => Option[Json] =
+    json => json.hcursor.downField("hotel_type")
+              .withFocus(_.mapString(_ => hotelType)).top
+
+  def updateStars(stars: Int): Json => Option[Json] =
+    json => json.hcursor.downField("stars")
+      .withFocus(_ => Json.fromInt(stars)).top
+
+  // modifying multiple fields using cursors and for comprehension
+
+  val result0_1 = for {
+    step1 <- updateName("The Lost Wonder")(doc)
+    step2 <- updateHotelType("premium") (step1)
+    jsonResult <- updateStars(5) (step2)
+  } yield jsonResult
+
+  println(result0_1.fold("parser error")(_.toString))
+
+  import JsonManipulationAPI._
 
   val result: String = json.map(isEnabled(true).andThen(_.toString()))
     .fold(_ => "invalid json", r => r)
@@ -43,7 +85,7 @@ object CirceOpticDemo extends App {
       .andThen(_.toString())
   ).fold(_ => "invalid json", r => r)
 
-  println(result)
+  println(result1)
 
   // way 2: using a method
   val result2: String = json.map(update(
@@ -71,11 +113,12 @@ object CirceOpticDemo extends App {
         .andThen(setLocationAddress(address))
         .andThen(setLocationZipCode(zipCode))
 
-    def updateFold(name: String,
-                   hotelType: String,
-                   stars: Int,
-                   address: String,
-                   zipCode: String): JsonAction =
+    // transformation API using fold
+    def updateF(name: String,
+                hotelType: String,
+                stars: Int,
+                address: String,
+                zipCode: String): JsonAction =
       fold(_)(List(
         setName(name),
         setHotelType(hotelType),
@@ -84,12 +127,12 @@ object CirceOpticDemo extends App {
         setLocationZipCode(zipCode)
       ))
 
-    def fold(initial: Json)(transformations: List[JsonAction]): Json = {
+    private def fold(initial: Json)(transformations: List[JsonAction]): Json = {
 
       @tailrec
-      def go(json: Json, tranformations: List[JsonAction]): Json = {
-        if (tranformations.isEmpty) json
-        else go(tranformations.head(json), tranformations.tail)
+      def go(json: Json, transformations: List[JsonAction]): Json = {
+        if (transformations.isEmpty) json
+        else go(transformations.head(json), transformations.tail)
       }
 
       go(initial, transformations)
